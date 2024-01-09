8bd5078c33cd41436827d24771df20f9ff1d7fd9
bipinkumar <bipinkumar@192.168.1.3>
2024-01-10 02:35:58 +0530

    second commit of 2024

---
+++ src/javaProgramms/SwapNumber.java
@@ -0,0 +1,23 @@
+package javaProgramms;
+
+public class SwapNumber {
+
+	public static void main(String[] args) {
+		// Swap the given 2 number
+		//Ex: a = 20, b=50, so print a=50 and b=20
+		
+		int a = 10;
+		
+		int b = 30;
+		
+		int c = a;
+		
+		a=b;
+		
+		b=c;
+		
+		System.out.println("The Swapped Numbers are " + a + " " + b);
+
+	}
+
+}
